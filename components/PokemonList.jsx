bc63f988640758953bfcbd93db6eb967e1e5e290
gustavo-ghcs <ghcs@cin.ufpe.br>
2023-07-23 18:48:38 -0300

    Resolvendo Bugs

--- components/PokemonList.jsx
+++ components/PokemonList.jsx
@@ -4,14 +4,20 @@ import './PokemonList.css';
 
 const PokemonList = () => {
   const [pokemonList, setPokemonList] = useState([]);
-  const [page, setPage] = useState(1); // Estado para controlar a página atual
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
     const fetchPokemonList = async () => {
       try {
         const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/?limit=20&offset=${(page - 1) * 20}`);
         const pokemonData = response.data.results;
-        setPokemonList((prevList) => [...prevList, ...pokemonData]); // Adicionando novos Pokémon à lista existente
+
+        if (page === 1) {
+          setPokemonList(pokemonData);
+        } else {
+          setPokemonList((prevList) => [...prevList, ...pokemonData]);
+        }
+        
       } catch (error) {
         console.error('Error fetching Pokémon list:', error);
       }
@@ -20,37 +26,41 @@ const PokemonList = () => {
     fetchPokemonList();
   }, [page]);
 
-  // Função para detectar quando o usuário chegou ao final da lista
   const handleScroll = () => {
     if (
       window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight
     ) {
-      setPage((prevPage) => prevPage + 1); // Incrementando o número da página para buscar mais Pokémon
+      setPage((prevPage) => prevPage + 1);
     }
   };
 
   useEffect(() => {
-    // Adicionando um observador de eventos de rolagem
     window.addEventListener('scroll', handleScroll);
 
-    // Removendo o observador quando o componente é desmontado
     return () => {
       window.removeEventListener('scroll', handleScroll);
     };
   }, []);
 
+  useEffect(() => {
+    const listContainer = document.getElementById('pokemon-list-container');
+    listContainer.addEventListener('scroll', handleScroll);
+
+    return () => {
+      listContainer.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <div>
+    <div id="pokemon-list-container">
       <h2>Lista de Pokémons</h2>
-      <div className="pokemon-list-container"> {/* Contêiner com barra de rolagem */}
-        <ul>
-          {pokemonList.map((pokemon) => (
-            <li key={pokemon.name}>
-              {pokemon.name} (ID: {getPokemonId(pokemon.url)})
-            </li>
-          ))}
-        </ul>
-      </div>
+      <ul>
+        {pokemonList.map((pokemon) => (
+          <li key={pokemon.name}>
+            {pokemon.name} (ID: {getPokemonId(pokemon.url)})
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
